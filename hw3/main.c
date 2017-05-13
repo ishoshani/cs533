9c07e69fe5dae5e4fb246589fd04210549bb8fbb
isho <idoshoshani@gmail.com>
2017-05-12 17:26:40 -0700

    testmode

--- hw3/main.c
+++ hw3/main.c
@@ -1,29 +1,54 @@
 #include "scheduler.h"
+#include "async."
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-void read_from_reader(void * pn){
-  printf("goooo\n");
+void print_nth_prime(void * pn){
+  printf("goooo");
+	int n = *(int *) pn;
   int c = 1, i = 1;
-  char buf[20];
+  while(c <= n) {
+    ++i;
+    int j, isprime = 1;
+    for(j = 2; j < i; ++j) {
+      if(i % j == 0) {
+        isprime = 0;
+        break;
+      }
+    }
+    if(isprime) {
+      ++c;
+    }
+    yield();
+  }
+  printf("%dth prime: %d\n", n, i);
+
+}
+
+void read_from_reader(void * pn){
+  char buf[5];
   size_t nbytes = sizeof(buf-1);
   ssize_t bytesread;
-  int filedesc = open("reader.txt",O_RDONLY);
+  int filedesc = STDIN_FILENO;
   bytesread= read(filedesc, buf, nbytes);
   for (i = 0; i < nbytes; i++) {
     printf("%c", buf[i]);
   }
+  printf("\n");
 }
+
+
 
 
 int main(void) {
   scheduler_begin();
 
   int n1 = 20000, n2 = 10000, n3 = 30000;
-  thread_fork(read_from_reader, &n1);
-  thread_fork(read_from_reader, &n2);
-  thread_fork(read_from_reader, &n3);
+  thread_fork(print_nth_prime, &n1);
+  thread_fork(print_nth_prime, &n2);
+  thread_fork(print_nth_prime, &n3);
+
 
   scheduler_end();
 }
