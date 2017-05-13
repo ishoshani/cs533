5f76fb99ada412a2c5d32a74e8a12e4deed5801f
isho <idoshoshani@gmail.com>
2017-05-12 17:19:13 -0700

    testmode

--- hw3/main.c
+++ hw3/main.c
@@ -2,17 +2,16 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-void print_nth_prime(void * pn){
+void read_from_reader(void * pn){
   printf("goooo\n");
   int c = 1, i = 1;
   char buf[20];
   size_t nbytes = sizeof(buf-1);
   ssize_t bytesread;
   int filedesc = open("reader.txt",O_RDONLY);
-  bytesread= (filedesc, buf, nbytes);
+  bytes read= (filedesc, buf, nbytes);
   for (i = 0; i < nbytes; i++) {
     printf(" %02hhX", buf[i]);
-    // On older compilers use --> printf(" %02X", (unsigned) ptr[i]);
   }
 }
 
@@ -21,9 +20,9 @@ int main(void) {
   scheduler_begin();
 
   int n1 = 20000, n2 = 10000, n3 = 30000;
-  thread_fork(print_nth_prime, &n1);
-  thread_fork(print_nth_prime, &n2);
-  thread_fork(print_nth_prime, &n3);
+  thread_fork(read_from_reader, &n1);
+  thread_fork(read_from_reader, &n2);
+  thread_fork(read_from_reader, &n3);
 
   scheduler_end();
 }
