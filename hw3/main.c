d0505130a71c7466c8dc211a4c84769eb9e6826d
isho <idoshoshani@gmail.com>
2017-05-12 17:03:27 -0700

    testmode

--- hw3/main.c
+++ hw3/main.c
@@ -10,7 +10,7 @@ void print_nth_prime(void * pn){
   ssize_t bytesread;
   int filedesc = open("reader.txt",O_RDONLY);
   bytesread= (filedesc, buf, nbytes);
-  buf[19]='\0';
+  buf[19]= 0;
   printf("%s ", buf);
 }
 
