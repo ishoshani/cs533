2ee84820d01b94012c9c99088ff133dce8080967
isho <idoshoshani@gmail.com>
2017-05-12 17:40:16 -0700

    testmode

--- hw3/async.c
+++ hw3/async.c
@@ -7,9 +7,16 @@
 
 ssize_t read_wrap(int fd, void * buf, size_t count){
   struct aiocb * controlblock = malloc(sizeof(struct aiocb));
+  ssize_t bytesread;
   controlblock->aio_fildes=fd;
   controlblock->aio_buf= buf;
   controlblock->aio_nbytes= count;
   controlblock->aio_sigevent.sigev_notify=SIGEV_NONE;
-  return aio_read(controlblock);
+  aio_read(controlblock);
+  int errorval;
+  while((errorval = aio_error(controlblock))==EINPROGRESS){
+    yield();
+  }
+  return aio_return(controlblock);
+
 }
