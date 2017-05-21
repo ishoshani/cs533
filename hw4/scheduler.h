8b619b64546e7947c662dd3316f9a0f99d71d0b6
isho <idoshoshani@gmail.com>
2017-05-21 15:38:32 -0700

    first draft hw4

--- hw4/scheduler.h
+++ hw4/scheduler.h
@@ -44,11 +44,11 @@ void thread_switch(struct thread * old, struct thread * new);
 void thread_start(struct thread * old, struct thread * new);
 //void thread_wrap();
 
-void mutex_init(struct mutex * mtx)
-void mutex_lock(struct mutex * mtx)
-void mutex_unlock(struct mutex * mtx)
+void mutex_init(struct mutex * mtx);
+void mutex_lock(struct mutex * mtx);
+void mutex_unlock(struct mutex * mtx);
 
-void condition_init(struct condition * cond)
-void condition_wait(struct condition * cond, struct mutex * mtx)
-void condition_signal(struct condition * cond)
-void condition_broadcast(struct condition * cond)
+void condition_init(struct condition * cond);
+void condition_wait(struct condition * cond, struct mutex * mtx);
+void condition_signal(struct condition * cond);
+void condition_broadcast(struct condition * cond);
