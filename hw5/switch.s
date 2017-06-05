ac9efe48cea9e73452a120a804711b4d15196e14
isho <idoshoshani@gmail.com>
2017-06-05 13:30:07 -0700

    hw5

---
+++ hw5/switch.s
@@ -0,0 +1,31 @@
+#void_switch(void * old, void * new)
+.globl thread_switch
+thread_switch:
+	pushq %rbx #push all callee-save registers
+	pushq %rbp
+	pushq %r12
+	pushq %r13
+	pushq %r14
+	pushq %r15
+	movq %rsp, (%rdi)		#save stack pointer into old's thread control block
+	movq (%rsi), %rsp 	#move new threads control block onto the stack pointer
+	popq %r15 #pop all the callee-save registers
+	popq %r14
+	popq %r13
+	popq %r12
+	popq %rbp
+	popq %rbx
+	ret
+#void thread_start(void * old_thread, void * new_thread
+.globl thread_start
+thread_start:
+	pushq %rbx #push all callee-save registers
+	pushq %rbp
+	pushq %r12
+	pushq %r13
+	pushq %r14
+	pushq %r15
+	movq %rsp, (%rdi)		#save stack pointer into old's thread control block
+	movq (%rsi), %rsp 		#move new threads control block onto the stack pointer
+	jmp thread_wrap
+
